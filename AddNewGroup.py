35ff807b149bb88cdc6aff3153f7986d368dfff0
Mary <7333581>
2019-03-22 23:34:44 +0300

    Создание нового вспомогательного класса. Добавление параметров

--- AddNewGroup.py
+++ AddNewGroup.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from group import Group
 import unittest, time, re
 
 class AddNewGroup(unittest.TestCase):
@@ -15,9 +13,9 @@ class AddNewGroup(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_start_page(wd)
-        self.Login(wd)
+        self.Login(wd, username="admin", password="secret")
         self.open_group_page(wd)
-        self.create_new_group(wd)
+        self.create_new_group(wd, Group(name="New_group", footer="create group", header="new_group_header"))
         self.return_group_page(wd)
         self.logout(wd)
 
@@ -27,12 +25,18 @@ class AddNewGroup(unittest.TestCase):
     def return_group_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def create_new_group(self, wd):
+    def create_new_group(self, wd, group):
         wd.find_element_by_name("new").click()
         #fiil group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("new_group")
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         #send form
         wd.find_element_by_name("submit").click()
 
@@ -42,12 +46,12 @@ class AddNewGroup(unittest.TestCase):
     def open_start_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
-    def Login(self, wd):
+    def Login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@type='submit']").click()
 
     def is_element_present(self, how, what):
