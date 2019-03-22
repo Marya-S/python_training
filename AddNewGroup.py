19b80f5b09da9a1d1cd72dfb803dee16cc7ac5f2
Mary <7333581>
2019-03-22 23:02:24 +0300

    Разбитие на методы

--- AddNewGroup.py
+++ AddNewGroup.py
@@ -14,24 +14,42 @@ class AddNewGroup(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
+        self.open_start_page(wd)
+        self.Login(wd)
+        self.open_group_page(wd)
+        self.create_new_group(wd)
+        self.return_group_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_group_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def create_new_group(self, wd):
+        wd.find_element_by_name("new").click()
+        #fiil group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("new_group")
+        #send form
+        wd.find_element_by_name("submit").click()
+
+    def open_group_page(self, wd):
+        wd.find_element_by_link_text("groups").click()
+
+    def open_start_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    def Login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@type='submit']").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("new_group")
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-        wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-    
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
