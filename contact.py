36f471086e05676e5cdeeb8673881800edb95634
Mary <7333581>
2019-03-26 22:19:13 +0300

    Создание классов-помощников и класса-менеджера

--- contact.py
+++
@@ -1,6 +0,0 @@
-class Contact:
-    def __init__(self, firstname, lastname, homenumber, mobilenumber):
-        self.firstname = firstname
-        self.lastname = lastname
-        self.homenumber = homenumber
-        self.mobilenumber = mobilenumber
