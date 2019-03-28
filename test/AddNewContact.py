143defedda9d13707909f6b28995aa4b19ded1d7
Mary <7333581>
2019-03-28 23:14:36 +0300

    Создание общей фикстуры в файле conftest.py

--- test/AddNewContact.py
+++ test/AddNewContact.py
@@ -1,15 +1,7 @@
 from model.contact import Contact
-from fixture.application import Application
-import pytest
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
-def test_untitled_test_case(app):
+def test_create_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Irina", lastname="Ivanova", homenumber="84997774747", mobilenumber="89051114512"))
     app.session.logout()
