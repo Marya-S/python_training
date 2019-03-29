252fb0b6543587ecbe2a3324056336f8f835e3bd
Mary <7333581>
2019-03-29 22:33:09 +0300

    Добавлены тесты update и delete для контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -18,6 +18,27 @@ class ContactHelper:
         wd.find_element_by_name("mobile").send_keys(contact.mobilenumber)
         wd.find_element_by_name("submit").click()
 
+    def delete(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//input[@type ='checkbox'][1]").click()
+        wd.find_element_by_xpath("//tr[@name ='entry'][1]//img[@title='Edit']").click()
+        wd.find_element_by_xpath("//input[@value = 'Delete']").click()
+
+    def edit(self, contact):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//input[@type ='checkbox'][1]").click()
+        wd.find_element_by_xpath("//tr[@name ='entry'][1]//img[@title='Edit']").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.homenumber)
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact.mobilenumber)
+        wd.find_element_by_xpath("//input[@value = 'Update']").click()
 
     def open_new_contact(self):
         wd = self.app.wd
