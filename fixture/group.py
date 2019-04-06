93b9050dc043514f6f581dcdf2a001d06257848d
Mary <7333581>
2019-04-06 21:45:28 +0300

    Оптимизация перехода между страницами

--- fixture/group.py
+++ fixture/group.py
@@ -51,8 +51,7 @@ class GroupHelper:
         self.open_group_page()
         return len(wd.find_elements_by_xpath("//span[@class='group']/input[1]"))
 
-
-
     def open_group_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
