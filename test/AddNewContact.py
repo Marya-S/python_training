619f56040f2619a087a3fdd32f6fd4ddec5200ed
Mary <7333581>
2019-05-03 19:26:24 +0300

    Добавление параметризации в тесты

--- test/AddNewContact.py
+++ test/AddNewContact.py
@@ -1,21 +1,27 @@
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_create_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname="", lastname="", homenumber="", mobilenumber="")]  + [
+    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 15),
+            homenumber=random_string("home", 15), mobilenumber=random_string("mobile", 15))
+    for i in range(3)
+]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_create_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Irina", lastname="Ivanova", homenumber="84997774747", mobilenumber="89051114512")
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
-
-def test_create_second_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Sergey", lastname="Petrov", homenumber="84951234312", mobilenumber="890311143443")
-    app.contact.create(contact)
-    new_contacts = app.contact.get_contact_list(contact)
-    assert len(old_contacts) + 1 == len(new_contacts)
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- test/AddNewGroup.py
+++ test/AddNewGroup.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
+import pytest
+import random
+import string
 
 
-def test_add_group(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Group(name="", footer="", header="")] + [
+    Group(name=random_string("group_name", 10), header=random_string("group_header", 15),
+          footer=random_string("group_footer", 15))
+    for i in range(3)
+]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="New_group2", footer="create group2", header="new_group_header2")
     app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
